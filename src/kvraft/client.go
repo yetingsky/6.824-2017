077148a1829d3819260a7389595ecb9c03a0e20c
XingDL <xingdl2007@gmail.com>
2017-12-19 16:35:34 +0800

    Lab3: kv raft PartA task1

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,21 @@
 package raftkv
 
-import "labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"math/big"
+	"crypto/rand"
+	"labrpc"
+	"time"
+)
 
+var clients = make(map[int64]bool)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+
+	leader int   // remember last leader
+	seq    int   // RPC sequence number
+	id     int64 // client id
 }
 
 func nrand() int64 {
@@ -17,10 +25,28 @@ func nrand() int64 {
 	return x
 }
 
+func generateID() int64 {
+	for {
+		x := nrand()
+		if clients[x] {
+			continue
+		}
+		clients[x] = true
+		return x
+	}
+}
+
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
+
 	// You'll have to add code here.
+	ck.leader = len(servers)
+	ck.seq = 1
+	ck.id = generateID()
+
+	DPrintf("Clerk: %d\n", ck.id)
+
 	return ck
 }
 
@@ -37,8 +63,36 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
+	DPrintf("Clerk: Get: %s\n", key)
 	// You will have to modify this function.
+	cnt := len(ck.servers)
+	for {
+		args := &GetArgs{Key: key, ClientID: ck.id, SeqNo: ck.seq}
+		reply := new(GetReply)
+
+		ck.leader %= cnt
+		// rpc timeout: 1s
+		timeout := time.NewTimer(1000 * time.Millisecond)
+		done := make(chan bool, 1)
+		go func() {
+			ok := ck.servers[ck.leader].Call("RaftKV.Get", args, reply)
+			done <- ok
+		}()
+		select {
+		case <-timeout.C:
+			ck.leader++
+			continue
+		case ok := <-done:
+			if !timeout.Stop() {
+				<-timeout.C
+			}
+			if ok && !reply.WrongLeader {
+				ck.seq++
+				return reply.Value
+			}
+			ck.leader++
+		}
+	}
 	return ""
 }
 
@@ -53,7 +107,37 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
+	DPrintf("Clerk: PutAppend: %q => (%q,%q) from: %d\n", op, key, value, ck.id)
 	// You will have to modify this function.
+	cnt := len(ck.servers)
+	for {
+		args := &PutAppendArgs{Key: key, Value: value, Op: op, ClientID: ck.id, SeqNo: ck.seq}
+		reply := new(PutAppendReply)
+		ck.leader %= cnt
+
+		// rpc timeout: 1s
+		timeout := time.NewTimer(1000 * time.Millisecond)
+		done := make(chan bool, 1)
+		go func() {
+			ok := ck.servers[ck.leader].Call("RaftKV.PutAppend", args, reply)
+			done <- ok
+		}()
+
+		select {
+		case <-timeout.C:
+			ck.leader++
+			continue
+		case ok := <-done:
+			if !timeout.Stop() {
+				<-timeout.C
+			}
+			if ok && !reply.WrongLeader && reply.Err == OK {
+				ck.seq++
+				return
+			}
+			ck.leader++
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -17,11 +17,21 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key      string
+	Value    string
+	Op       string // "Get", "Put" or "Append"
+	ClientID int64  // client id
+	SeqNo    int    // request sequence number
+}
+
+type LastReply struct {
+	latest int         // latest seq
+	seq    int         // last apply
+	reply  interface{} // last reply
 }
 
 type RaftKV struct {
@@ -33,15 +43,186 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
-}
+	db        map[string]string
+	notifyChs map[int]chan struct{}
 
+	// shutdown chan
+	shutdownCh chan struct{}
+
+	// duplication detection table
+	duplicate map[int64]*LastReply
+}
 
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	// not leader
+	if _, isLeader := kv.rf.GetState(); !isLeader {
+		reply.WrongLeader = true
+		reply.Err = ""
+		return
+	}
+
+	DPrintf("[%d]: leader %d receive rpc: Get(%q).\n", kv.me, kv.me, args.Key)
+
+	kv.mu.Lock()
+	// duplicate get request
+	if dup, ok := kv.duplicate[args.ClientID]; ok {
+		if args.SeqNo == dup.seq {
+			kv.mu.Unlock()
+			reply.WrongLeader = false
+			reply.Err = OK
+			reply.Value = dup.reply.(*GetReply).Value
+			return
+		}
+	} else {
+		kv.duplicate[args.ClientID] = new(LastReply)
+	}
+
+	// update latest seq no
+	kv.duplicate[args.ClientID].latest = args.SeqNo
+
+	cmd := Op{Key: args.Key, Op: "Get", ClientID: args.ClientID, SeqNo: args.SeqNo}
+	index, term, _ := kv.rf.Start(cmd)
+	ch := make(chan struct{})
+	kv.notifyChs[index] = ch
+	kv.mu.Unlock()
+
+	reply.WrongLeader = false
+	reply.Err = OK
+
+	// wait for Raft to complete agreement
+	select {
+	case <-ch:
+		// lose leadership
+		curTerm, isLeader := kv.rf.GetState()
+		// TODO: what if still leader, but different term ?
+		if !isLeader || term != curTerm {
+			reply.WrongLeader = true
+			reply.Err = ""
+			return
+		}
+
+		kv.mu.Lock()
+		delete(kv.notifyChs, index)
+		if value, ok := kv.db[args.Key]; ok {
+			reply.Value = value
+		} else {
+			reply.Err = ErrNoKey
+		}
+		kv.mu.Unlock()
+	case <-kv.shutdownCh:
+	}
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	// not leader
+	if _, isLeader := kv.rf.GetState(); !isLeader {
+		reply.WrongLeader = true
+		reply.Err = ""
+		return
+	}
+
+	DPrintf("[%d]: leader %d receive rpc: PutAppend(%q => (%q,%q), (%d-%d).\n", kv.me, kv.me,
+		args.Op, args.Key, args.Value, args.ClientID, args.SeqNo)
+
+	kv.mu.Lock()
+	// duplicate put/append request
+	if dup, ok := kv.duplicate[args.ClientID]; ok {
+		// filter duplicate
+		if args.SeqNo <= dup.latest {
+			kv.mu.Unlock()
+			reply.WrongLeader = false
+			reply.Err = OK
+			return
+		}
+	} else {
+		kv.duplicate[args.ClientID] = new(LastReply)
+	}
+
+	kv.duplicate[args.ClientID].latest = args.SeqNo
+	// new request
+	cmd := Op{Key: args.Key, Value: args.Value, Op: args.Op, ClientID: args.ClientID, SeqNo: args.SeqNo}
+	index, term, _ := kv.rf.Start(cmd)
+	ch := make(chan struct{})
+	kv.notifyChs[index] = ch
+	kv.mu.Unlock()
+
+	reply.WrongLeader = false
+	reply.Err = OK
+
+	// wait for Raft to complete agreement
+	select {
+	case <-ch:
+		kv.mu.Lock()
+		delete(kv.notifyChs, index)
+		kv.mu.Unlock()
+
+		// lose leadership
+		curTerm, isLeader := kv.rf.GetState()
+		// TODO: what if still leader, but different term?
+		if !isLeader || term != curTerm {
+			reply.WrongLeader = true
+			reply.Err = ""
+			return
+		}
+	case <-kv.shutdownCh:
+		return
+	}
+}
+
+// applyDaemon receive applyMsg from Raft layer, apply to Key-Value state machine
+// then notify related client if is leader
+func (kv *RaftKV) applyDaemon() {
+	for {
+		select {
+		case <-kv.shutdownCh:
+			DPrintf("[%d]: server %d is shutting down.\n", kv.me, kv.me)
+			return
+		case msg, ok := <-kv.applyCh:
+			if ok {
+				cmd := msg.Command.(Op)
+				kv.mu.Lock()
+				switch cmd.Op {
+				case "Get":
+					kv.duplicate[cmd.ClientID] = &LastReply{
+						seq: cmd.SeqNo,
+						reply: &GetReply{
+							WrongLeader: false,
+							Err:         OK,
+							Value:       kv.db[cmd.Key],
+						},
+					}
+				case "Put":
+					kv.db[cmd.Key] = cmd.Value
+					kv.duplicate[cmd.ClientID] = &LastReply{
+						seq:   cmd.SeqNo,
+						reply: nil,
+					}
+				case "Append":
+					kv.db[cmd.Key] += cmd.Value
+					kv.duplicate[cmd.ClientID] = &LastReply{
+						seq:   cmd.SeqNo,
+						reply: nil,
+					}
+				default:
+					DPrintf("[%d]: server %d receive invalid cmd: %v\n", kv.me, kv.me, cmd)
+				}
+				// notify channel
+				notifyCh := kv.notifyChs[msg.Index]
+				kv.mu.Unlock()
+
+				// notify client if it is or was leader
+				_, isLeader := kv.rf.GetState()
+				if isLeader && notifyCh != nil {
+					notifyCh <- struct{}{}
+				} else if notifyCh != nil {
+					// lose leadership, notify blocked chan receiver
+					close(notifyCh)
+				}
+			}
+		}
+	}
 }
 
 //
@@ -53,6 +234,7 @@ func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *RaftKV) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+	close(kv.shutdownCh)
 }
 
 //
@@ -83,6 +265,17 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	// store key-value pairs
+	kv.db = make(map[string]string)
+	kv.notifyChs = make(map[int]chan struct{})
+
+	// shutdown channel
+	kv.shutdownCh = make(chan struct{})
+
+	// duplication detection table: client->seq no.-> reply
+	kv.duplicate = make(map[int64]*LastReply)
+
+	go kv.applyDaemon()
 
 	return kv
 }
